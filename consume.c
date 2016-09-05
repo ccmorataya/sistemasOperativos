19a313c8d5a4854d29016ca341b919ad1a82f43c
Cristian Morataya <cris.morataya@gmail.com>
2016-09-05 15:44:10 -0600

    Corrección al programa consume.c

--- consume.c
+++ consume.c
@@ -6,12 +6,14 @@
 int main()
 {
     float a, b;
+    printf("Calculadora basica:\npara confirmar los datos utiliza [Enter]\nIngresa el primer numero a calcular\n");
     scanf("%f", &a);
+    printf("Ingresa el segundo numero a calcular\n");
     scanf("%f", &b);
-    printf("%s\n", getNames());
-    printf("%f\n", adition(a,b));
-    printf("%f\n", subtraction(a,b));
-    printf("%f\n", multiplication(a,b));
-    printf("%f\n", division(a,b));
+    printf("\nNombres de los integrantes:\n%s\n\n", getNames());
+    printf("Resultado de la suma: %f\n", adition(a,b));
+    printf("Resultado de la resta: %f\n", subtraction(a,b));
+    printf("Resultado de la multiplicación: %f\n", multiplication(a,b));
+    printf("Resultado de la división: %f\n\n", division(a,b));
     return 0;
 }
